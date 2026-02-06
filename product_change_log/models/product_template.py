fff5dc970c95dccce145fb57f01e2da906b69f8b
Bill Morris <billingmorris@gmail.com>
2026-02-05 21:38:27 -0500

    Add change logging to product.template model

---
+++ product_change_log/models/product_template.py
@@ -0,0 +1,31 @@
+from odoo import models
+
+TRACK_FIELDS = [
+    'name',
+    'default_code',
+    'list_price',
+    'standard_price',
+    'barcode',
+    'categ_id',
+    'type',
+]
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    def write(self, vals):
+        for record in self:
+            for field in TRACK_FIELDS:
+                if field in vals:
+                    old_value = record[field]
+                    new_value = vals[field]
+
+                    if old_value != new_value:
+                        self.env['product.change.log'].create({
+                            'product_id': record.id,
+                            'field_name': field,
+                            'old_value': str(old_value),
+                            'new_value': str(new_value),
+                        })
+
+        return super(ProductTemplate, self).write(vals)
